a20f4733e34a28e4f970ac8517383023f8468e89
ManuelSandoval98 <julio.sandoval.l@tecsup.edu.pe>
2024-10-16 21:18:19 -0500

    no message

--- SandovalSnapchat/AppDelegate.swift
+++ SandovalSnapchat/AppDelegate.swift
@@ -7,14 +7,20 @@
 
 import UIKit
 import FirebaseCore
+import GoogleSignIn
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Configura Firebase
         FirebaseApp.configure()
         return true
     }
-}
 
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+      return GIDSignIn.sharedInstance.handle(url)
+    }
+}
--- SandovalSnapchat/iniciarSesionViewController.swift
+++ SandovalSnapchat/iniciarSesionViewController.swift
@@ -7,29 +7,82 @@
 
 import UIKit
 import FirebaseAuth
+import GoogleSignIn
+import FirebaseCore
 
 class iniciarSesionViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    
-    @IBAction func iniciarSesionTapped(_ sender: Any) {
-            Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
-                print("Intentando Iniciar Sesion")
-                if error != nil {
-                    print("Se presento el siguiente error: \(error)")
-                } else {
-                    print("Inicio de sesion exitoso")
-                }
-            }
-        }
-
+    @IBOutlet weak var anonymousLoginButton: UIButton!
+    @IBOutlet weak var signInButton: GIDSignInButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            print("Intentando Iniciar Sesion")
+            if let error = error {
+                print("Se presentó el siguiente error: \(error)")
+            } else {
+                print("Inicio de sesión exitoso")
+            }
+        }
+    }
 
+    @IBAction func anonymousLoginTapped(_ sender: Any) {
+        signInAnonymously()
+    }
+    
+    
+    
+    
+    @IBAction func btnGmail(_ sender: Any) {
+        guard let clientID = FirebaseApp.app()?.options.clientID else {
+            print("No se encontró el Client ID de Firebase.")
+            return
+        }
+        // Crear la configuración para Google Sign-In
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+        // Iniciar el flujo de inicio de sesión con Google
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
+            if let error = error {
+                print("Error en la autenticación con Google: \(error.localizedDescription)")
+                return
+            }
+            guard let user = result?.user,
+                  let idToken = user.idToken?.tokenString else {
+                print("No se pudo obtener el ID Token.")
+                return
+            }
+            // Crear la credencial de Firebase con el ID Token de Google
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                           accessToken: user.accessToken.tokenString)
+            // Autenticar con Firebase usando la credencial de Google
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
+                    print("Error al autenticar con Firebase: \(error.localizedDescription)")
+                } else {
+                    print("¡Inicio de sesión exitoso con Google!")
+                }
+            }
+        }
+    }
+    
+    
+
+    func signInAnonymously() {
+        Auth.auth().signInAnonymously() { (authResult, error) in
+            if let error = error {
+                print("Error signing in: \(error.localizedDescription)")
+                return
+            }
+            if let user = authResult?.user {
+                print("Usuario autenticado con ID: \(user.uid)")
+            }
+        }
+    }
 }
-
