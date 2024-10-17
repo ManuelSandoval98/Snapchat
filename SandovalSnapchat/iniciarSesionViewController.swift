a06220c98e094f4c91a3293abe336bc50ccf5879
ManuelSandoval98 <julio.sandoval.l@tecsup.edu.pe>
2024-10-16 19:41:43 -0500

    lab_terminado

---
+++ SandovalSnapchat/iniciarSesionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  SandovalSnapchat
+//
+//  Created by Manuel Sandoval on 16/10/24.
+//
+
+import UIKit
+import FirebaseAuth
+
+class iniciarSesionViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+            Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+                print("Intentando Iniciar Sesion")
+                if error != nil {
+                    print("Se presento el siguiente error: \(error)")
+                } else {
+                    print("Inicio de sesion exitoso")
+                }
+            }
+        }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
